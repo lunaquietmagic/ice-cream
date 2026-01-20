f82ce1ccef2172f08bc534e7ec428ebecc9a47bc
velsbit <raimchannel@gmail.com>
2026-01-20 22:46:57 +0400

    chat html and css

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask
+from config import config
 
 from .routes.home import home_page
 from .routes.login import login_page
 from .routes.profile import profile_page
 from .routes.register import register_page
+from .routes.chat import chat_page
 
-app = Flask(__name__)
+def create_app(config_name='default'):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
 
-app.register_blueprint(home_page)
-app.register_blueprint(login_page)
-app.register_blueprint(profile_page)
-app.register_blueprint(register_page)
+    app.register_blueprint(home_page)
+    app.register_blueprint(login_page)
+    app.register_blueprint(profile_page)
+    app.register_blueprint(register_page)
+    app.register_blueprint(chat_page)
 
-
-if __name__ == "__main__":
-    app.run()
+    return app
---
+++ app/routes/chat.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+chat_page = Blueprint('chat_page', __name__)
+
+@chat_page.route("/chat")
+def view_chat():
+    return render_template("chat.html")
